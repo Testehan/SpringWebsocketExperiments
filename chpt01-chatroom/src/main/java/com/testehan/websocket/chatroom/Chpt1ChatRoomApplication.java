9f4b32377652d71cc2c211133d8795b3d6c10c75
DanTestehan <tdan89@yahoo.com>
2024-07-18 20:20:20 +0100

    chpt01-chatroom - started working on chat project

---
+++ chpt01-chatroom/src/main/java/com/testehan/websocket/chatroom/Chpt1ChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.testehan.websocket.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chpt1ChatRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt1ChatRoomApplication.class, args);
+	}
+
+}
