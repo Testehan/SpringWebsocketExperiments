a8f2d883f091d55e0bdb2f7fa876c27b5df3da37
DanTestehan <tdan89@yahoo.com>
2024-07-19 20:20:20 +0100

    chpt03-hello-world-stomp - started new prj

---
+++ chpt03-hello-world-stomp/src/main/java/com/testehan/websocket/hello/Chpt3HelloWorldWithStompApplication.java
@@ -0,0 +1,20 @@
+package com.testehan.websocket.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+
+	If you start the app go to the link from below..press on the "connect" button
+		http://localhost:8080/
+
+*/
+
+@SpringBootApplication
+public class Chpt3HelloWorldWithStompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt3HelloWorldWithStompApplication.class, args);
+	}
+
+}
