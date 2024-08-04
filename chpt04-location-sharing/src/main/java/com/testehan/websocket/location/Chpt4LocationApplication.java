c8596b7faf1ba7293d0e70f2ef9318a2883408d4
DanTestehan <tdan89@yahoo.com>
2024-08-04 20:20:20 +0100

    chpt04-location-sharing - new prj

---
+++ chpt04-location-sharing/src/main/java/com/testehan/websocket/location/Chpt4LocationApplication.java
@@ -0,0 +1,20 @@
+package com.testehan.websocket.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+
+	If you start the app go to the link from below..press on the "connect" button
+		http://localhost:8080/
+
+*/
+
+@SpringBootApplication
+public class Chpt4LocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chpt4LocationApplication.class, args);
+	}
+
+}
